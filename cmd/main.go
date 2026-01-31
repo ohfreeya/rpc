caf8bbe0f1ee80ad7515ced1af280f02dade7f49
Zachary Zhao <zachary.zhao@accuhit.net>
2026-01-31 22:07:12 +0800

    feat: 實作簡單的 api 及測試，新增 ci yaml

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"rpc/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	r := gin.Default()
+
+	r.GET("/ping", handler.Ping)
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ internal/handler/handle_test.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPing(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	t.Run("success", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+		c.Request, _ = http.NewRequest("GET", "/ping", nil)
+
+		Ping(c)
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
+}
